33cf19764203ed57c1d2735487d35ce767e5995c
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2023-09-06 08:43:11 +0600

    added pass gen and pass chk

--- password-generator.py
+++ password-generator.py
@@ -27,20 +27,75 @@ def symb_check(password):
             return True
     return False
 
-def generate_password(length:int, lower:bool=True, upper:bool=None, numbers:bool=True, symbols:bool=None):
-    combination: str=string.ascii_lowercase + string.digits
+def generate_password(length:int, upper:bool=None, symbols:bool=None):
+    combination = string.digits + string.ascii_lowercase
     
     
 
     if upper:
+        print("upper +")
         combination += string.ascii_uppercase
 
-    if numbers:
-        combination += string.digits
-
     if symbols:
+        print("symbols +")
         combination += string.punctuation
 
-pwd = getpass.getpass("Password:")
+    c_len = len(combination)
+    new_pass = ""
 
-print(upper_check(pwd))
+    for i in range(length):
+        new_pass += combination[secrets.randbelow(c_len)]
+    
+    return new_pass
+
+def pass_check(string):
+    if upper_check(string):
+        print("Password has upper case letters.")
+
+    if lower_check(string):
+        print("Password has lower case letters.")
+
+    if symb_check(string):
+        print("Password has special characters.")
+
+    if digit_check(string):
+        print("Password has numbers.")
+
+
+def main():
+    print("Choose one operation below:")
+    print("1. Generate Password.")
+    print("2. Password checker.")
+    print()
+    choice = int(input("Please enter your choice... "))
+
+    if choice==1:
+        print()
+        l = int(input("Please enter the length of the password:"))
+        print()
+        u = input("Do you want uppercase [Y/N]: ").lower()
+        s = input("Do you want special characters [Y/N]: ")
+        print()
+
+        if u=="y":
+            u=True
+        else:
+            u=False
+        
+        if s=="y":
+            s=True
+        else:
+            s=False
+
+        new = generate_password(l, u, s)
+        print(new)
+
+    
+    if choice==2:
+        print()
+        pwd = getpass.getpass(prompt="Enter Password:")
+        print()
+        pass_check(pwd)
+
+if __name__=="__main__":
+    main()
